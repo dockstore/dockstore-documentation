b2979f2067fd651cefa41aaac50e10a92be10517
NatalieEO <nperez9@ucsc.edu>
2020-07-13 10:25:57 -0700

    add scripts

---
+++ docs/scripts/deactivate-version.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# First command line argument is your readthedocs token. https://readthedocs.org/accounts/tokens/
+# Second command line argument is the name of your branch. If you name your branch to include a '/' you will need change to "-" and uppercase becomes lowercase.
+# "feature/testBranch" -> "feature-testbranch"
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+# Activate version
+curl -X PATCH "https://readthedocs.org/api/v3/projects/dockstore/versions/$2/" -H "Authorization: Token $1" -H "Content-Type: application/json" -d @deactivate-body.json

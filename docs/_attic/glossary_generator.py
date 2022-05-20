7f121ac7f8b8aad083f07cf3d009c0f88bbcd88b
aofarrel <aofarrel@ucsc.edu>
2022-05-20 14:39:00 -0700

    Print name of file that generates dictionary.rst

--- docs/_attic/glossary_generator.py
+++ docs/_attic/glossary_generator.py
@@ -22,5 +22,5 @@ for glossary_object in gc.get_objects():
     if isinstance(glossary_object, GlossEntry):
         dockstore_dictionary.add_entry(glossary_object)
 dockstore_dictionary.sort_entries()
-dockstore_dictionary.write_toc(contents_outfile, "txt")
-dockstore_dictionary.write_glossary(glossary_outfile)
+dockstore_dictionary.write_toc(contents_outfile, format="txt", skipSource=True)
+dockstore_dictionary.write_glossary(glossary_outfile, sourcefile=__file__)

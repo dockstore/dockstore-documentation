f68e9fb5d5b64900ebb5653fb8f336aa70e7ea25
Flynn Hembroff <flynnhembroff@gmail.com>
2022-06-15 18:56:02 -0400

    Got discourse-topic-check.sh working

--- discourse-topic-check.sh
+++ discourse-topic-check.sh
@@ -1,5 +1,33 @@
 
-echo "develop second"
-git diff --name-only --diff-filter=A ${CIRCLE_BRANCH}..develop
-echo "develop first"
-git diff --name-only --diff-filter=A develop..${CIRCLE_BRANCH}
+# Determines if a file has a discourse topic
+function containsDiscourseTopic {
+  grep -A1 "^.. discourse::" $fileToCheck | tail -n1 | grep -E "^( )*:topic_identifier:( )+[0-9]+" > /dev/null
+  if [ $? != 0 ]
+  then
+    echo "${fileToCheck} does not have a discourse topic"
+    return 1
+  else
+    return 0
+  fi
+}
+
+RETURN_VALUE=0
+BASE_BRANCH=develop
+CURRENT_BRANCH=
+DOES_NOT_REQUIRE_DISCOURSE_TOPIC=no-discourse-required.txt
+
+for file in $(git diff --name-only develop.. | grep -E "*\.rst" | grep -Fvxf $DOES_NOT_REQUIRE_DISCOURSE_TOPIC)
+do
+  fileToCheck=$file
+  if ! containsDiscourseTopic
+  then
+    RETURN_VALUE=1
+  fi
+done
+
+if [ $RETURN_VALUE != 0 ]
+then
+  echo "If your files do not require a discourse topic you can add them to ${DOES_NOT_REQUIRE_DISCOURSE_TOPIC}"
+fi
+
+exit $RETURN_VALUE

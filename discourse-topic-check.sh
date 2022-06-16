caa37a27f167b8ea7ce5a9ef2bec1c04b756460f
Flynn Hembroff <flynnhembroff@gmail.com>
2022-06-15 21:24:29 -0400

    Correcrted error

--- discourse-topic-check.sh
+++ discourse-topic-check.sh
@@ -35,7 +35,7 @@ DOES_NOT_REQUIRE_DISCOURSE_TOPIC=no-discourse-required.txt
 pr=$(echo $CIRCLE_PULL_REQUEST | sed 's+https://github.com+https://api.github.com/repos+' | sed 's/pull/pulls/')
 branch=$(curl -s $pr | jq '.base.ref') # This gets a variable such as "develop"
 branch=${branch:1:-1}  # This removes the "" from branch
-for file in $(git diff --name-only $"branch"..| grep -E "*\.rst" | grep -Fvxf $DOES_NOT_REQUIRE_DISCOURSE_TOPIC)
+for file in $(git diff --name-only "$branch"..| grep -E "*\.rst" | grep -Fvxf $DOES_NOT_REQUIRE_DISCOURSE_TOPIC)
 do
   fileToCheck=$file
   if ! containsDiscourseTopic

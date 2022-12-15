ddcd2644ebfd6593c4aab628010b0181c2ce4212
Stephen Von Worley <svonworl@ucsc.edu>
2022-12-15 09:19:59 -0800

    rename script

--- add-discourse-topic-to-all.sh
+++ add-discourse-topic-to-all.sh
@@ -4,6 +4,6 @@ source helpers.sh
 
 for file in $(find . -name '*.rst' | sed 's/^.\///' | grep -Fvxf no-discourse-topic-required.txt); do
     if ! containsDiscourseTopic "$file"; then
-        ./add-topic-to-rst.sh "$file"
+        ./add-discourse-topic.sh "$file"
     fi
 done

86d9656f2845150ac7dfeccc67cd68a0616b6b86
Denis Yuen <denis.yuen@oicr.on.ca>
2024-08-21 11:22:21 -0400

    syntax

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ extensions = ['myst_parser', 'sphinxcontrib.discourse']
 
 linkcheck_anchors = False
 linkcheck_retries = 5
-linkcheck_report_timeouts_as_broken = false
+linkcheck_report_timeouts_as_broken = False
 linkcheck_ignore = [
     'http://localhost:8080/ga4gh/wes/v1', 'https://support.orcid.org/hc/en-us/articles/360006894754-Edit-works',
     'https://dx.doi.org/10.12688/f1000research.10137.1', 'https://dockstore.org/api/',
